e304d2a338c2307cee3b6045c52c1f94ca346bfb
edinadelic <edinadelic83@gmail.com>
2020-07-10 13:22:02 +0200

    Adding pagination

---
+++ DatingApp-SPA/src/app/_models/pagination.ts
@@ -0,0 +1,9 @@
+export interface Pagination {
+    currentPage: number;
+    itemsPerPage: number;
+    totalItems: number;
+}
+export class PaginatedResult<T>{
+    result: T;
+    pagination: Pagination;
+}
--- DatingApp.API/Helpers/Extensions.cs
+++ DatingApp.API/Helpers/Extensions.cs
@@ -1,5 +1,7 @@
 using System;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace DatingApp.API.Helpers
 {
@@ -10,6 +12,15 @@ namespace DatingApp.API.Helpers
           response.Headers.Add("Application-Error", message);
           response.Headers.Add("Access-Control-Expose-Headers", "Application-Error");
           response.Headers.Add("Access-Control-Allow-Origin", "*");
+        }
+
+        public static void AddPaggination(this HttpResponse response, int currentPage, int itemsPerPage, int totalItems, int totalPages)
+        {
+          var camelCaseFormatter = new JsonSerializerSettings();
+          camelCaseFormatter.ContractResolver = new CamelCasePropertyNamesContractResolver();
+          var paginationHeader = new PaginationHeader(currentPage, itemsPerPage, totalItems, totalPages);
+          response.Headers.Add("Pagination", JsonConvert.SerializeObject(paginationHeader, camelCaseFormatter));
+          response.Headers.Add("Access-Control-Allow-Origin", "Pagination");
         }
 
         public static int CalculateAge(this DateTime theDateTime){
