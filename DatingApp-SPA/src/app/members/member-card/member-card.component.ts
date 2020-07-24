892d78a588b88f1bad99091e1c92176cb57d7285
Edina Delic <edinadelic83@gmail.com>
2020-07-24 13:10:37 +0200

    Adding like functionality

--- DatingApp-SPA/src/app/members/member-card/member-card.component.ts
+++ DatingApp-SPA/src/app/members/member-card/member-card.component.ts
@@ -1,16 +1,34 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { User } from '../../_models/user';
+import { UserService } from 'src/app/_services/user.service';
+import { AuthService } from 'src/app/_services/auth.service';
+import { AlertifyService } from 'src/app/_services/alertify.service';
 
 @Component({
   selector: 'app-member-card',
   templateUrl: './member-card.component.html',
-  styleUrls: ['./member-card.component.css']
+  styleUrls: ['./member-card.component.css'],
 })
 export class MemberCardComponent implements OnInit {
- @Input() user: User;
-  constructor() { }
+  @Input() user: User;
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+    private alertify: AlertifyService
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  sendLike(id: number) {
+    this.userService
+      .sendLike(this.authService.decodedToken.nameid, id)
+      .subscribe(
+        data => {
+          this.alertify.success('You liked: ' + this.user.knownAs);
+        },
+        (error) => {
+          this.alertify.error(error);
+        }
+      );
   }
-
 }
